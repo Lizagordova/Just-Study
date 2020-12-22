19473fa4f95019e24509036d797b7be5b84fdfe8
lizunchik_737 <lizunchik_737@mail.ru>
2020-12-22 09:10:45 +0300

    лишнее

--- SuperSoft/client/src/components/Users/AddUser.tsx
+++ SuperSoft/client/src/components/Users/AddUser.tsx
@@ -30,7 +30,6 @@ export class AddUser extends React.Component<IUsersProps>{
     roleDropdownOpen: boolean;
     role: Role;
     notSaved: boolean;
-    saved: boolean;
 
     constructor() {
         // @ts-ignore
@@ -88,7 +87,6 @@ export class AddUser extends React.Component<IUsersProps>{
                 toggle={() => this.toggleAddUserWindow()}>
                 <i className="fa fa-window-close cool-close-button" aria-hidden="true"
                 onClick={() => this.toggleAddUserWindow()}/>
-                {this.saved && <Alert color="success">Всё успешно сохранилось!</Alert>}
                 {this.notSaved && <Alert color="danger">Что-то пошло не так и пользователь не сохранился!</Alert>}
                 <ModalHeader closeButton>ДОБАВЛЕНИЕ НОВОГО ПОЛЬЗОВАТЕЛЯ</ModalHeader>
                 <ModalBody>
@@ -173,7 +171,6 @@ export class AddUser extends React.Component<IUsersProps>{
                     this.notSaved = true;
                 } else {
                     this.notSaved = false;
-                    this.saved = true;
                     this.addUserWindowOpen = false;
                 }
             });
