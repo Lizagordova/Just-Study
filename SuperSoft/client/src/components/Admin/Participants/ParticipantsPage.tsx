cb8498181c743d7183df069df6f9e0eb50e356f6
lizunchik_737 <lizunchik_737@mail.ru>
2021-01-24 12:22:54 +0300

    дополнила ParticipantsPage

--- SuperSoft/client/src/components/Admin/Participants/ParticipantsPage.tsx
+++ SuperSoft/client/src/components/Admin/Participants/ParticipantsPage.tsx
@@ -4,7 +4,7 @@ import CourseStore from "../../../stores/CourseStore";
 import { makeObservable, observable } from "mobx";
 import { UserViewModel } from "../../../Typings/viewModels/UserViewModel";
 import UserStore from "../../../stores/UserStore";
-import { Button, Alert } from "reactstrap";
+import { Button, Alert, Label } from "reactstrap";
 
 class IParticipantsPageProps {
     courseStore: CourseStore;
@@ -14,6 +14,7 @@ class IParticipantsPageProps {
 @observer
 class ParticipantsPage extends Component<IParticipantsPageProps> {
     participants: UserViewModel[];
+    restUsers: UserViewModel[];
     saved: boolean;
     notSaved: boolean;
 
@@ -24,39 +25,67 @@ class ParticipantsPage extends Component<IParticipantsPageProps> {
             participants: observable,
             saved: observable,
             notSaved: observable,
+            restUsers: observable
         });
     }
 
     componentDidMount(): void {
         let usersByCourse = this.props.courseStore.usersByCourse;
         let users = this.props.userStore.users;
-        this.participants = users.filter(u => usersByCourse
+        let participants = users.filter(u => usersByCourse
             .map(uc => uc.userId)
-            .includes(u.id))
+            .includes(u.id));
+        this.restUsers = users.filter(function(u) {
+            return !participants.includes(u);
+        });
+        this.participants = participants;
     }
 
     renderCurrentParticipants(participants: UserViewModel[]) {
         return(
             <>
                 {participants.map(p => {
-                    
+                    return(
+                        <div className="row justify-content-center">
+                            <Label>
+                                {p.lastName} {p.firstName}
+                            </Label>
+                            <i style={{marginLeft: '98%', width: '2%'}}
+                               onClick={() => this.deleteParticipant(p)}
+                               className="fa fa-window-close" aria-hidden="true"/>
+                        </div>
+                    );
                 })}
             </>
         );
     }
 
-    renderRestUsers() {
+    renderRestUsers(users: UserViewModel[]) {
         return(
-            <></>
+            <>
+                {users.map(u => {
+                    return(
+                        <div className="row justify-content-center">
+                            <Label>
+                                {u.lastName} {u.firstName}
+                            </Label>
+                            <i style={{marginLeft: '98%', width: '2%'}}
+                               onClick={() => this.addParticipant(u)}
+                               className="fa fa fa-plus" aria-hidden="true"/>
+                        </div>
+                    )
+                })}
+            </>
         );
     }
 
-    renderAttachButton() {
+    renderSaveButton() {
         return(
-            <Button>
+            <Button color="primary"
+                onClick={(e) => this.save()}>
                 Сохранить
             </Button>
-        )
+        );
     }
 
     renderSavedDetails() {
@@ -70,21 +99,27 @@ class ParticipantsPage extends Component<IParticipantsPageProps> {
 
     renderDividingLine() {
         return(
-            
+            <>
+                <hr />
+                <span style={{fontSize: "1.5em"}}>
+                    Добавить:
+                </span>
+            </>
         )
     }
-    
+
     render() {
         return(
             <>
                 {this.renderSavedDetails()}
                 {this.renderCurrentParticipants(this.participants)}
+                {this.renderSaveButton()}
                 {this.renderDividingLine()}
-                {this.renderRestUsers()}
+                {this.renderRestUsers(this.restUsers)}
             </>
         );
     }
-    
+
     save() {
         let participantsIds = this.participants.map(p => p.id);
         this.props.courseStore.AddOrUpdateParticipantsList(participantsIds, this.props.courseStore.choosenCourse.id)
@@ -92,6 +127,19 @@ class ParticipantsPage extends Component<IParticipantsPageProps> {
                 this.saved = status === 200;
                 this.notSaved = status !== 200;
             });
+    }
+
+    deleteParticipant(participant: UserViewModel) {
+        let participants = this.participants;
+        this.participants = participants.filter(p => p.id !== participant.id);
+    }
+
+    addParticipant(user: UserViewModel) {
+        let participants = this.participants;
+        let restUsers = this.restUsers;
+        participants.push(user);
+        this.participants = participants;
+        this.restUsers = restUsers.filter(u => u.id !== user.id);
     }
 }
 
