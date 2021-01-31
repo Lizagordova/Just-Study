f2d6ffcc33c2d4d71246e521c5b486e5503a81ee
lizunchik_737 <lizunchik_737@mail.ru>
2021-01-31 16:33:58 +0300

    реализация LogRepository

--- SuperSoft.Persistence/Repositories/LogRepository.cs
+++ SuperSoft.Persistence/Repositories/LogRepository.cs
@@ -1,13 +1,42 @@
-﻿using SuperSoft.Domain.Models;
+﻿using System.Data;
+using Dapper;
+using SuperSoft.Domain.Models;
 using SuperSoft.Domain.Repositories;
+using SuperSoft.Persistence.Extensions;
+using SuperSoft.Persistence.Helpers;
+using SuperSoft.Persistence.Models.Dto;
+using SuperSoft.Persistence.Services.MapperService;
 
 namespace SuperSoft.Persistence.Repositories
 {
 	public class LogRepository : ILogRepository
 	{
+		private readonly MapperService _mapper;
+		private const string AddLogSp = "LogRepository_AddLog";
+
+		public LogRepository(
+			MapperService mapper)
+		{
+			_mapper = mapper;
+		}
+
 		public void AddLog(Log log)
 		{
-			throw new System.NotImplementedException();
+			var conn = DatabaseHelper.OpenConnection();
+			var param = GetAddLogParam(log);
+			conn.Query(AddLogSp, param, commandType: CommandType.StoredProcedure);
+			DatabaseHelper.CloseConnection(conn);
+		}
+
+		private DynamicTvpParameters GetAddLogParam(Log log)
+		{
+			var param = new DynamicTvpParameters();
+			var tvp = new TableValuedParameter("log", "UDT_Log");
+			var logUdt = _mapper.Map<Log, LogUdt>(log);
+			tvp.AddObjectAsRow(logUdt);
+			param.Add(tvp);
+
+			return param;
 		}
 	}
 }
