3298c8a205c9763408e5355c177df1230c4eee05
lizunchik_737 <lizunchik_737@mail.ru>
2021-02-05 19:12:57 +0300

    ещё хранимки в WordRepository

---
+++ SuperSoft.Persistence/Scripts/Procedures/WordRepository/[WordRepository_GetUserWordProgress].sql
@@ -0,0 +1,29 @@
+﻿CREATE PROCEDURE [WordRepository_GetUserWordProgress]
+	@userId INT,
+	@wordId INT
+AS
+BEGIN
+	DECLARE @userWord [UDT_User_Word];
+
+	INSERT
+	INTO @userWord (
+		[UserId],
+		[WordId],
+		[CountOfAttempts],
+		[RightAnswers],
+		[Status],
+		[Answer]
+	)
+	SELECT
+		[UserId],
+		[WordId],
+		[CountOfAttempts],
+		[RightAnswers],
+		[Status],
+		[Answer]
+	FROM [User_Word]
+	WHERE [UserId] = @userId
+		AND [WordId] = @wordId;
+
+	SELECT * FROM @userWord;
+END
---
+++ SuperSoft.Persistence/Scripts/Procedures/WordRepository/[WordRepository_GetWordOfADay].sql
@@ -0,0 +1,50 @@
+﻿CREATE PROCEDURE [WordRepository_GetWordOfADay]
+	@date DATETIME2,
+	@courseId INT
+AS
+BEGIN
+	DECLARE @word [UDT_Word];
+	DECLARE @examples [UDT_Example];
+
+	DECLARE @wordId INT = (
+		SELECT [WordId] 
+		FROM [WordOfADay] 
+		WHERE [CourseId] = @courseId 
+			AND [Date] = @date
+	);
+
+	INSERT
+	INTO @word (
+		[Id],
+		[Word],
+		[EnglishMeaning],
+		[RussianMeaning],
+		[PartOfSpeech]
+	)
+	SELECT
+		[Id],
+		[Word],
+		[EnglishMeaning],
+		[RussianMeaning],
+		[PartOfSpeech]
+	FROM [Word]
+	WHERE [Id] = @wordId;
+
+	INSERT
+	INTO @examples (
+		[Id],
+		[WordId],
+		[Example]
+	)
+	SELECT
+		[ex].[Id],
+		@wordId,
+		[ex].[Example]
+	FROM [Example] AS [ex]
+	JOIN [Word_Example] AS [we]
+	ON [ex].[Id] = [we].[ExampleId]
+	WHERE [we].[WordId] = @wordId;
+
+	SELECT * FROM @word;
+	SELECT * FROM @examples;
+END
